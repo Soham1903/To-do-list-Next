80de52d5be5653ea7bbee5056aced8eca44a24cb
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2024-12-28 19:54:24 +0530

    Intial Files

---
+++ Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="flex py-3 flex-wrap justify-around">
+      <h1 className="text-lg font-bold">Todo App</h1>
+      {/* <ul className="flex gap-[40px] text-m">
+        <li className="mx-4">Home</li>
+        <li className="mx-4">About</li>
+        <li className="mx-4">Contact</li>
+        <li className="mx-4">Products</li>
+      </ul> */}
+    </div>
+  );
+}
+
+export default Navbar;
